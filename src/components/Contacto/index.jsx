35765444d6fe0669b06d61efc1bb00e318d50ebe
Elias Lee <eliaslee87@gmail.com>
2021-03-09 00:16:13 -0300

    Update ContactoComponent

--- src/components/Contacto/index.jsx
+++ src/components/Contacto/index.jsx
@@ -4,36 +4,36 @@ import './style.css'
 const ContactoComponent = () => {
     return (
        <div className="f-Gothic">
-            <Row className="mb-4">
-                <Col xs="6" className="d-flex justify-content-end align-items-center p-0">
+            <div className="mb-4">
+                <div xs="6" className="d-flex justify-content-center align-items-center p-0">
                     <p className="m-0 font-weight-7">신랑에게 연락하기</p>
-                </Col>
-                <Col xs="3" className="d-flex justify-content-center align-items-center p-0">
-                    <a href="tel:+5491140746199">
-                        <span className="btn-tel-blue p-2 rounded-circle material-icons">call</span>
-                    </a>
-                </Col>
-                <Col xs="3" className="d-flex justify-content-start align-items-center p-0">
-                    <a href="https://wa.me/5491140746199">
-                        <span className="btn-tel-grey p-2 rounded-circle material-icons">textsms</span>
-                    </a>
-                </Col>
-            </Row>
-            <Row className="mb-4">
-                <Col xs="6" className="d-flex justify-content-end align-items-center p-0">
+                    <div className="mx-3">
+                        <a href="tel:+5491140746199">
+                            <span className="btn-tel-blue p-2 rounded-circle material-icons">call</span>
+                        </a>
+                    </div>
+                    <div className="mx-3">
+                        <a href="https://wa.me/5491140746199">
+                            <span className="btn-tel-grey p-2 rounded-circle material-icons">textsms</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="mb-4">
+                <div xs="6" className="d-flex justify-content-center align-items-center p-0">
                     <p className="m-0 font-weight-7">신부에게 연락하기</p>
-                </Col>
-                <Col xs="3" className="d-flex justify-content-center align-items-center p-0">
-                    <a href="tel:+5491134834433">
-                        <span className="btn-tel-pink p-2 rounded-circle material-icons">call</span>
-                    </a>
-                </Col>
-                <Col xs="3" className="d-flex justify-content-start align-items-center p-0">
-                    <a href="https://wa.me/5491134834433">
-                        <span className="btn-tel-grey p-2 rounded-circle material-icons">textsms</span>
-                    </a>
-                </Col>
-            </Row>
+                    <div className="mx-3">
+                        <a href="tel:+5491134834433">
+                            <span className="btn-tel-pink p-2 rounded-circle material-icons">call</span>
+                        </a>
+                    </div>
+                    <div className="mx-3">
+                        <a href="https://wa.me/5491134834433">
+                            <span className="btn-tel-grey p-2 rounded-circle material-icons">textsms</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
 
             <div className="py-3" style={{backgroundColor: "#f4f4f4"}}>
                 <h6 className="m-0 font-size-s text-center font-weight-7">혼주에게 연락하기</h6>
